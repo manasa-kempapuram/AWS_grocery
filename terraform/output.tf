3f811ec4a0be6e8ae59d3e6356fee2c404f68839
Manasa Shivaram <manasashivaram@mac.speedport.ip>
2025-07-04 23:41:33 +0200

    terraform applied

---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+output "instance_public_ip" {
+  value = aws_instance.grocery_app.public_ip
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "key_pair_name" {
+  description = "EC2 key pair name used for SSH"
+  type        = string
+  default      = "Manasa_key" 
+}
+
